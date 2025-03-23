5d01fa29cc71b28680dae7f784b79a98dbb62ecc
John P. McGlone <jp@jpmcglone.com>
2025-03-23 10:01:47 -0400

    Nuxt UI / Tailwind setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
   modules: [
     '@nuxt/content',
     '@nuxt/eslint',
@@ -12,5 +12,6 @@ export default defineNuxtConfig({
     '@nuxt/scripts',
     '@nuxt/test-utils',
     '@nuxt/ui'
-  ]
+  ],
+  css: ['~/assets/css/main.css']
 })
