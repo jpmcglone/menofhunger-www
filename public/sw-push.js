7068eb63959513d4cddfd83a9bab0997708027d2
JP McGlone <jp@jpmcglone.com>
2026-02-09 22:55:35 -0500

    Fixes

--- public/sw-push.js
+++ public/sw-push.js
@@ -5,7 +5,7 @@
  */
 
 // IMPORTANT: bump this whenever caching logic changes so old caches are purged.
-self.__MOH_SW_VERSION = 'moh-sw-dev-1770695311644'const CACHE_PREFIX = 'moh-sw'
+self.__MOH_SW_VERSION = 'moh-sw-dev-1770695563608'const CACHE_PREFIX = 'moh-sw'
 const NUxT_ASSETS_CACHE = `${CACHE_PREFIX}:nuxt:${self.__MOH_SW_VERSION}`
 const STATIC_ASSETS_CACHE = `${CACHE_PREFIX}:static:${self.__MOH_SW_VERSION}`
 
